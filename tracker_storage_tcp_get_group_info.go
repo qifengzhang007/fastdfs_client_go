f9e193fd450328e0c9973ee7d724dddb36cf9fd2
张qifeng <1990850157@qq.com>
2025-11-11 17:31:46 +0800

    update: 修正变量名.

--- tracker_storage_tcp_get_group_info.go
+++ tracker_storage_tcp_get_group_info.go
@@ -4,36 +4,36 @@ import "errors"
 
 // GetGroupInfo  获取远程服务器的文件信息
 // @remoteFileId 远程服务器的文件Id
-func (c *trackerServerTcpClient) GetGroupInfo(groupName string) (ResGroupInfo GroupInfo, err error) {
+func (c *trackerServerTcpClient) GetGroupInfo(groupName string) (resGroupInfo GroupInfo, err error) {
 
 	if len(groupName) < 1 {
-		return ResGroupInfo, errors.New("groupName 不能为空")
+		return resGroupInfo, errors.New("groupName 不能为空")
 	}
 
 	storageServInfo, err := c.getStorageInfoByTracker(TRACKER_PROTO_CMD_SERVICE_QUERY_STORE_WITH_GROUP_ONE, groupName, "")
 	if err != nil {
-		return ResGroupInfo, err
+		return resGroupInfo, err
 	}
 	queryGroupInfo := &storageGetGroupInfoHeaderBody{}
 	// 请求时需要的业务参数赋值
 	queryGroupInfo.groupName = groupName
 
 	if err = c.sendCmdToStorageServer(queryGroupInfo, storageServInfo); err != nil {
-		return ResGroupInfo, err
+		return resGroupInfo, err
 	}
-	ResGroupInfo.respGroupName = queryGroupInfo.respGroupName
-	ResGroupInfo.resptotalMb = queryGroupInfo.resptotalMb
-	ResGroupInfo.respFreeMb = queryGroupInfo.respFreeMb
-	ResGroupInfo.respReservedMb = queryGroupInfo.respReservedMb
-	ResGroupInfo.resptrunkFreeMb = queryGroupInfo.resptrunkFreeMb
-	ResGroupInfo.respServerCount = queryGroupInfo.respServerCount
-	ResGroupInfo.respServerPort = queryGroupInfo.respServerPort
-	ResGroupInfo.respReadableServerCount = queryGroupInfo.respReadableServerCount
-	ResGroupInfo.respWritableServerCount = queryGroupInfo.respWritableServerCount
-	ResGroupInfo.respCurrentWriteServerCount = queryGroupInfo.respCurrentWriteServerCount
-	ResGroupInfo.respstorePathCount = queryGroupInfo.respstorePathCount
-	ResGroupInfo.respsubdirCountPerPath = queryGroupInfo.respsubdirCountPerPath
-	ResGroupInfo.respcurrentTrunkFileId = queryGroupInfo.respcurrentTrunkFileId
+	resGroupInfo.respGroupName = queryGroupInfo.respGroupName
+	resGroupInfo.resptotalMb = queryGroupInfo.resptotalMb
+	resGroupInfo.respFreeMb = queryGroupInfo.respFreeMb
+	resGroupInfo.respReservedMb = queryGroupInfo.respReservedMb
+	resGroupInfo.resptrunkFreeMb = queryGroupInfo.resptrunkFreeMb
+	resGroupInfo.respServerCount = queryGroupInfo.respServerCount
+	resGroupInfo.respServerPort = queryGroupInfo.respServerPort
+	resGroupInfo.respReadableServerCount = queryGroupInfo.respReadableServerCount
+	resGroupInfo.respWritableServerCount = queryGroupInfo.respWritableServerCount
+	resGroupInfo.respCurrentWriteServerCount = queryGroupInfo.respCurrentWriteServerCount
+	resGroupInfo.respstorePathCount = queryGroupInfo.respstorePathCount
+	resGroupInfo.respsubdirCountPerPath = queryGroupInfo.respsubdirCountPerPath
+	resGroupInfo.respcurrentTrunkFileId = queryGroupInfo.respcurrentTrunkFileId
 
-	return ResGroupInfo, nil
+	return resGroupInfo, nil
 }
